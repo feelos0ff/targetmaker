9639587f39e3a43e546e9b02cbc182b2ecf25347
priora <kostia_k@bk.ru>
2014-10-23 15:48:34 +0400

    added some code with elastic

--- buildering/initDB.py
+++ buildering/initDB.py
@@ -9,7 +9,19 @@ from buildering.models import engine, Countries, Regions, Cities
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.sql.expression import and_
 import re
+from pyes import *
 
+def toElastic():
+    es = ES('127.0.0.1:9200')
+    rsc = open('../rsc/locations.csv')
+    i = 0
+    for line in rsc:
+        res = es.index(index="test-index", doc_type='tweet', id=i,body={'geo':line})
+        i += 1
+        print(res['created'])
+
+
+        
 
 
 if __name__ == '__main__':
@@ -17,10 +29,42 @@ if __name__ == '__main__':
     
     Session = sessionmaker(bind=engine)
     session = Session()
+    mapping = {u'country_code': {'boost': 1.0,
+                                'index': 'analyzed',
+                                'store': 'yes',
+                                'type': u'string',
+                                "term_vector": "with_positions_offsets"},
+                u'country_name': {'boost': 1.0,
+                                'index': 'analyzed',
+                                'store': 'yes',
+                                'type': u'string',
+                                "term_vector": "with_positions_offsets"},
+                u'region_code': {'boost': 1.0,
+                                'index': 'analyzed',
+                                'store': 'yes',
+                                'type': u'string',
+                                "term_vector": "with_positions_offsets"},
+                u'region_name': {'boost': 1.0,
+                                'index': 'analyzed',
+                                'store': 'yes',
+                                'type': u'string',
+                                "term_vector": "with_positions_offsets"},
+                u'city_name': {'boost': 1.0,
+                                'index': 'analyzed',
+                                'store': 'yes',
+                                'type': u'string',
+                                "term_vector": "with_positions_offsets"},
+    }
+  
+    es = ES('127.0.0.1:9200')
+    es.indices.create_index("geo-index")
+    es.indices.put_mapping("geo", {'properties': mapping}, "geo-index")
     
     geo = open('../rsc/locations.csv')
-    
+    num = 0
     for line in geo:
+        num +=1
+        line = line.replace('"', '')
         line = [i[1:].strip() for i in re.findall(r',[^,]*', line)]
 
         region = Regions()
@@ -37,10 +81,15 @@ if __name__ == '__main__':
             region.name  = line[5]
         
         city.name    = line[6]
-       
-        countryInDB = session.query(Countries).filter(and_(Countries.code == country.code, 
-                                                         Countries.name == country.name))
         
+        res = es.index({'country_code' : country.code, 'country_name' : country.name,
+                        'region_code' : region.code, 'region_name' : region.name, 'city_name':line[6]},"geo-index", "geo", num)
+        
+        print res
+        '''
+        countryInDB = session.query(Countries).filter(and_(Countries.code == country.code, 
+                                                           Countries.name == country.name))
+         
         if countryInDB.count() > 0 :
                        
             country = countryInDB[0]
@@ -65,3 +114,6 @@ if __name__ == '__main__':
 
         session.add(city)
         session.commit()  
+        '''
+    es.indices.refresh()
+    geo.close()
--- parsers/location.py
+++ parsers/location.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 22 окт. 2014 г.
 
@@ -5,6 +6,9 @@ Created on 22 окт. 2014 г.
 '''
 from buildering.models import Countries, Regions, Cities, engine
 from sqlalchemy.orm import sessionmaker
+from elasticsearch.client import Elasticsearch
+from pyes.es import ES
+from pyes.query import TermsQuery, TermQuery, Search, WildcardQuery
 
 
 
@@ -17,6 +21,7 @@ class LocationParser(object):
         pass
     
     def parse(self, address):
+        
         Session = sessionmaker(bind=engine)
         session = Session() 
         
@@ -44,4 +49,23 @@ class LocationParser(object):
                     levelLoc.checkLoc(session, '', location)
                 continue
             break
+        
+
+def toElastic():
+    es = Elasticsearch()
+    rsc = open('../rsc/locations.csv')
+    i = 0
+    for line in rsc:
+        res = es.index(index="test-index", doc_type='tweet', id=i,body={'geo':line})
+        i += 1
+        print(res['created'])
+        
+es = ES('127.0.0.1:9200')
+
+res = TermQuery('country_code', 'us')
+res = es.search(query =res,indices ="geo-index")
+#res  = es.get("geo-index", "geo", 2)
+print res.total
+for x in res:
+    print x
         
