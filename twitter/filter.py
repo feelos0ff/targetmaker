0732a94a60b7b2f43bba1350bb3769df022ce443
feelos0ff <kostia_k@bk.ru>
2014-12-07 23:03:18 +0300

    first version of filter

--- twitter/filter.py
+++ twitter/filter.py
@@ -6,6 +6,7 @@ Created on 06 дек. 2014 г.
 '''
 
 import requests
+from parsers.text import TextProcess
 
 class TweeFilter(object):
     '''
@@ -13,15 +14,21 @@ class TweeFilter(object):
     '''
 
     def __init__(self, params):
-        '''
-        Constructor
-        '''
+        self.processor = TextProcess()
     
     def filter(self, tweet):
+        tweet = " OR ".join(self.processor.processing(tweet))
         query = {
                     "query_string" : {
-                        "default_field" : [""],
-                        "query" : "this AND that OR thus"
+                        "min_score": 0.5,
+                        "default_field" : ["brand","category"],
+                        "query" : tweet
                     }
                 }
-        return True
+        response = requests.get("http://localhost:9200/twitter/goods/_search", params=query)
+        
+        if response["hits"]["total"] > 1:
+            return True
+        
+        return False
+    
